493b63ca3193e9392027045de3ce76719911ed41
Neha2704 <nsrivastava.g@gmail.com>
2018-05-17 23:22:39 +0530

    Create property.js

---
+++ models/property.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const PropertySchema = new Schema({
+	propID: {
+		type: String,
+		required: [true, 'ID is required']
+	},
+	name: {
+		type: String
+	},
+	address: {
+		street: {
+			type: String,
+			//required: true
+		},
+		area: String,
+		city: String,
+		state: String
+	},
+	available: {	
+		type: Boolean,
+		default: true
+	},
+});
+
+const Property = mongoose.model('property', PropertySchema);
+
+module.exports = Property;
