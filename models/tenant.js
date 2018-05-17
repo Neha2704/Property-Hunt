5d42f1580bab0a2f4e215b0b5836e10d39c9ff92
Neha2704 <nsrivastava.g@gmail.com>
2018-05-17 23:22:10 +0530

    Create tenant.js

---
+++ models/tenant.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const TenantSchema = new Schema({
+	username: {
+		type: String,
+		required: [true, 'Username is required']
+	},
+	password: {
+		type: String,
+		required: [true, 'Password is required']
+	},
+	contactinfo: {
+		phone: {
+			type: String
+		},
+		email: {
+			type: String
+		}
+	},
+	complaints: {
+		complaint: String,
+		complaintPropID: String
+	},
+	properties: {
+		currentPropID: [String],
+		interestedPropID: [String]
+	}
+});
+
+const Tenant = mongoose.model('tenant', TenantSchema);
+
+module.exports = Tenant;
