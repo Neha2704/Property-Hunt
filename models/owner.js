5f876e553fab85d4bd674b01a4711f03e7b1aa55
Neha Srivastava <nsrivastava.g@gmail.com>
2018-05-24 20:29:16 +0530

    Update owner.js

--- models/owner.js
+++ models/owner.js
@@ -1,48 +1,35 @@
-const mongoose=require('mongoose');
-const Schema=mongoose.Schema;
-var bcrypt = require('bcrypt');
-var SALT_WORK_FACTOR = 10;
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
 
-const OwnerSchema= new Schema({
-	o_name: {
+const OwnerSchema = new Schema({
+	
+	// username
+	ownername: {
 		type: String,
-		required: [true, 'Name field is required']
+		required: [true, 'Username is required'],
+		unique: true
 	},
-	o_email: {
+	password: {
 		type: String,
-		required: [true, 'email field is required']
+		//required: [true, 'Password is required']
 	},
-	o_contact: {
-		type: Number,
-		required: [true, 'email field is required']
-	},
-	o_username: {
+	name: {
 		type: String,
-		required: [true, 'username field is required']
+		required: true
 	},
-	o_password: {
-		type: String,
-		required: [true, 'username field is required']
-	}
-});
-const Owner=mongoose.model('owner', OwnerSchema);
-
-OwnerSchema.pre('save', function(next){
-    var Owner = this;
-    if (!Owner.isModified('o_password')) return next();
- 
-    bcrypt.genSalt(SALT_WORK_FACTOR, function(err, salt){
-        if(err) return next(err);
- 
-        bcrypt.hash(Owner.o_password, salt, function(err, hash){
-            if(err) return next(err);
- 
-            Owner.o_password = hash;
-            next();
-        });
-    });
+	contactinfo: {
+		phone: {
+			type: String,
+			required: true
+		},
+		email: {
+			type: String
+		}
+	},
+	availablePropID: [String],
+	rentedPropID: [String]
 });
 
-//const Owner=mongoose.model('owner', OwnerSchema);
+const Owner = mongoose.model('owner', OwnerSchema);
 
-module.exports=Owner;
+module.exports = Owner;
