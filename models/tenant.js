eb24c1b9a29c5480d3e33869ab046a55973fddc6
Neha Srivastava <nsrivastava.g@gmail.com>
2018-05-24 20:30:11 +0530

    Update tenant.js

--- models/tenant.js
+++ models/tenant.js
@@ -1,48 +1,34 @@
-const mongoose=require('mongoose');
-const Schema=mongoose.Schema;
-var bcrypt = require('bcrypt');
-var SALT_WORK_FACTOR = 10;
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
 
-const TenantSchema= new Schema({
-	t_name: {
+const TenantSchema = new Schema({
+	
+	// username
+	tenantname: {
 		type: String,
-		required: [true, 'Name field is required']
+		required: [true, 'Username is required'],
+		unique: true
 	},
-	t_email: {
+	password: {
 		type: String,
-		required: [true, 'email field is required']
+		required: [true, 'Password is required']
 	},
-	t_contact: {
-		type: Number,
-		required: [true, 'email field is required']
-	},
-	t_username: {
+	name: {
 		type: String,
-		required: [true, 'username field is required']
+		required: true
 	},
-	t_password: {
-		type: String,
-		required: [true, 'username field is required']
-	}
-});
-const Tenant=mongoose.model('tenant', TenantSchema);
-
-TenantSchema.pre('save', function(next){
-    var Tenant = this;
-    if (!Tenant.isModified('t_password')) return next();
- 
-    bcrypt.genSalt(SALT_WORK_FACTOR, function(err, salt){
-        if(err) return next(err);
- 
-        bcrypt.hash(Tenant.t_password, salt, function(err, hash){
-            if(err) return next(err);
- 
-            Tenant.t_password = hash;
-            next();
-        });
-    });
+	contactinfo: {
+		phone: {
+			type: String
+		},
+		email: {
+			type: String
+		}
+	},
+	currentPropID: String,
+	interestedPropID: [String]
 });
 
-//const Owner=mongoose.model('owner', OwnerSchema);
+const Tenant = mongoose.model('tenant', TenantSchema);
 
-module.exports=Tenant;
+module.exports = Tenant;
